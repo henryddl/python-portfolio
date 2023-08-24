7a4342f4fd855f9dd66c1b640ca3430cd9e0dea6
Henry Diaz <henry.diaz@telus.com>
2023-08-23 22:07:22 -0600

    Day 23: Send email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 
 st.header("Contact me")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address")
+    content = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+    if button:
+        print("I was pressed")
